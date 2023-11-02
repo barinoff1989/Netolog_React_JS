685d0cfcbe099644b153f51b59cdb6bb5e5eb615
i.barinov <i.barinov@rosvodokanal.ru>
2023-11-02 16:38:10 +0300

    study1

---
+++ study1/task2/src/components/ShopItemClass/ShopItemClass.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import classes from './shopItemClass.css'
+
+export class ShopItemClass extends React.Component {
+  render() {
+    const{item} = this.props
+    return (
+      <div className="main-content">
+      <h2 className="h2">{item.brand}</h2>
+      <h1 className="h1">{item.title}</h1>
+      <h3 className="h3">{item.description}</h3>
+      <div className="description">
+      {item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
+        <button className="button">Добавить в корзину</button>
+      </div>
+    </div>
+    )
+  }
+}
+
+
+
